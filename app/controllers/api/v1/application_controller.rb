88c0794580b5b2e93e7f9eb666d206f75d72db73
Preeti Gupta <guptapreeti.india@gmail.com>
2014-09-17 00:53:45 +0530

    Added token based security in APIs

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::ApplicationController < ApplicationController
 
 	skip_before_filter :verify_authenticity_token
+	before_filter :restrict_access
+
 	def default_serializer_options
 	  {root: false}
 	end
+
+	private
+
+	def restrict_access
+	  api_key = ApiKey.where(token: params[:token]).first
+	  head :unauthorized unless api_key
+	end
+
 
 end
