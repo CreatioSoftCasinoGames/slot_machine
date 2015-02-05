736f94e855e3cedc6ce628c3d2eb86c2b550f430
Prashant Mishra <prashant.mishra@w3villa.com>
2015-02-05 17:04:45 +0530

    fix routes of assets api to machine/id/assets.json

--- app/controllers/api/v1/machines_controller.rb
+++ app/controllers/api/v1/machines_controller.rb
@@ -1,33 +1,36 @@
 class Api::V1::MachinesController < Api::V1::ApplicationController
-
-	def math_model_versions
-		@machine = Machine.where(id: params[:id]).first
-		render json: {
-			versions: @machine.math_models.as_json({
-				only: [:id, :version]
-			}),
-			active_math_model_id: @machine.active_math_model.try(:id)
-		}
-	end
-
-	def in_game_asset_versions
-		@machine = Machine.where(id: params[:id]).first
-		render json: {
-			versions: @machine.in_game_assets.as_json({
-				only: [:id, :version]
-			}),
-			active_in_game_asset_id: @machine.active_in_game_asset.try(:id)
-		}
-	end
-
-	def out_game_asset_versions
-		@machine = Machine.where(id: params[:id]).first
-		render json: {
-			versions: @machine.out_game_assets.as_json({
-				only: [:id, :version]
-			}),
-			active_out_game_asset_id: @machine.active_out_game_asset.try(:id)
-		}
-	end
+	
+	def assets
+    @machine = Machine.where(machine_number: params[:id]).first
+    @active_assets = @machine.active_asset.where(country: params[:country])
+    @check_math = @machine.active_asset.where(bundle_type: "MATH_MODEL_ASSET", country: params[:country])
+    @check_mini = @machine.active_asset.where(bundle_type: "MINI_GAME_ASSET", country: params[:country])
+    @check_graphic = @machine.active_asset.where(bundle_type: "GRAPHIC_ASSET", country: params[:country])
+    if @check_math.blank?
+        if @active_assets.present?
+            @active_assets = @active_assets + @machine.active_asset.where(bundle_type: "MATH_MODEL_ASSET", country: "World_Wide")
+        else
+            @active_assets = @machine.active_asset.where(bundle_type: "MATH_MODEL_ASSET", country: "World_Wide")
+        end
+    end
+    if @check_mini.blank?
+        if @active_assets.present?
+            @active_assets = @active_assets + @machine.active_asset.where(bundle_type: "MINI_GAME_ASSET", country: "World_Wide")
+        else
+            @active_assets = @machine.active_asset.where(bundle_type: "MINI_GAME_ASSET", country: "World_Wide")
+        end
+    end
+    if @check_graphic.blank?
+        if @active_assets.present?
+            @active_assets = @active_assets + @machine.active_asset.where(bundle_type: "GRAPHIC_ASSET", country: "World_Wide")
+        else
+            @active_assets = @machine.active_asset.where(bundle_type: "GRAPHIC_ASSET", country: "World_Wide")
+        end
+    end
+    render json: @active_assets.as_json({
+        only: [:version, :bundle_type, :file_file_size],
+        methods: [:file_updated_at, :url]
+    })
+  end
 
 end
