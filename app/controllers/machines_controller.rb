bb6911d742dfc51b9f555f02ff84b358976746b3
Prashant Mishra <prashant.mishra@w3villa.com>
2015-04-13 11:54:52 +0530

    Fix machine issues

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -69,6 +69,6 @@ class MachinesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def machine_params
-      params.require(:machine).permit(:name, :machine_number, :machine_type, :min_players, :max_players, :timeout, :seed_money)
+      params.require(:machine).permit(:name, :machine_number, :machine_type, :min_players, :max_players)
     end
 end
