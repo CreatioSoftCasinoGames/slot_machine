f3d4de16409d81885a1b4dc5accb7dd6a3e961e6
Amrendra Singh <amrendra.singh@w3villa.com>
2015-06-30 20:28:41 +0530

    Removed Bingo heading into SLOT

--- app/controllers/api/v1/client_bugs_controller.rb
+++ app/controllers/api/v1/client_bugs_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ClientBugsController < Api::V1::ApplicationController
 
 	def create
-		bug_type  = "BINGO - " + params[:bug_type]
+		bug_type  = "SLOT - " + params[:bug_type]
 	  @client_bug = ClientBug.new(exception: params[:exception], bug_type: bug_type, test_mode: params[:test_mode])
 		if @client_bug.save
 			render json: {
