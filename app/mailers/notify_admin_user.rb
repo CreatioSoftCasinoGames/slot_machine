4e5786e1315f98711926dcc420aaa339e820c971
Shashank Diwaker <amrendra.singh@w3villa.com>
2015-08-05 12:41:41 +0530

    Updated mails for bugs

--- app/mailers/notify_admin_user.rb
+++ app/mailers/notify_admin_user.rb
@@ -2,7 +2,7 @@ class NotifyAdminUser < ActionMailer::Base
 	default from: "bug@creatiosoft.com"
 
 	def send_error_mail(error)
-		@receivers = "amrendra@creatiosoft.com, ankit@creatiosoft.com, sagar@creatiosoft.com, ishank@creatiosoft.com, rishabh@creatiosoft.com"
+		@receivers = @productionRecievers = "rishabh@creatiosoft.com, bugcreatiosoft@gmail.com"
 		@subject = "Error While - "+error.bug_type
 		@body = "The following details will help you to track issues:\n\n"+error.exception
 		mail(to: @receivers, subject: @subject, body: @body)
