07a2846d70f6eaa6eea775bce2692cd7d8d51b92
CreatioSoft <application@creatiosoft>
2015-09-03 18:02:43 +0530

    some changes to how puma is managed

--- config/puma.rb
+++ config/puma.rb
@@ -3,13 +3,12 @@
 # start puma with:
 # RAILS_ENV=production bundle exec puma -C ./config/puma.rb
 
-application_path = '/home/w3villa/slot_machine/current'
+application_path = '/apps/slot_machine'
 railsenv = 'production'
 directory application_path
 environment railsenv
 daemonize true
-pidfile "#{application_path}/tmp/pids/puma-#{railsenv}.pid"
-state_path "#{application_path}/tmp/pids/puma-#{railsenv}.state"
-stdout_redirect "#{application_path}/log/puma-#{railsenv}.stdout.log", "#{application_path}/log/puma-#{railsenv}.stderr.log"
+pidfile "#{application_path}/current/tmp/pids/puma-#{railsenv}.pid"
+stdout_redirect "#{application_path}/current/log/puma-#{railsenv}.stdout.log", "#{application_path}/current/log/puma-#{railsenv}.stderr.log"
 threads 0, 16
 bind "unix:///tmp/slot_machine.sock"
