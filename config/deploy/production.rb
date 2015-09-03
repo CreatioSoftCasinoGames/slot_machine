adc00b2ace9813161f2ec7bf584a55470aca94ab
CreatioSoft <application@creatiosoft>
2015-09-03 09:01:09 +0530

    some changes to allow for deployment to new boxes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,12 @@
 # Settings for production server
-set :user, 'w3villa'
-server '52.27.233.56', :app, :web, :db, :primary => true
-server '52.27.111.246', :app, :web
-set :rvm_type, :user
+set :user, 'application'
+server 'slot-rails-1.slotapi.com', :app, :web
+server 'slot-rails-2.slotapi.com', :app, :web
+set :rvm_type, :system
 set :use_sudo, false
 set :deploy_via, :remote_cache
+set :copy_exclude, [ '.git' ]
 set :rvm_ruby_version, 'ruby-2.1.0'
 set :port, 22 
 
-set :deploy_to, "/home/#{user}/#{application}"
+set :deploy_to, "/apps/#{application}"
