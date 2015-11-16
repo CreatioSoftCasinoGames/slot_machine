22cd40051d1f9029e291bf6b1e0dd010bc5ee0c2
Amrendra Singh <amrendra.singh@w3villa.com>
2015-11-16 17:09:43 +0530

    SM-620 Update profile for client data as well

--- app/workers/update_profile_worker.rb
+++ app/workers/update_profile_worker.rb
@@ -5,14 +5,24 @@ class UpdateProfileWorker
 
 	def perform(data)
 		profile_data = JSON.parse(data)
-		user = User.find(profile_data["id"])
+		user = User.find(profile_data["id"]).first
 		if user.present?
-			user.update_attributes(
-				total_bet: 		profile_data["total_bet"],
-				coins_won: 		profile_data["coins_won"],
-				coins_lost: 	profile_data["coins_lost"],
-				total_spin: 	profile_data["total_spin"]
-			)
+			if profile_data["update_from"] == "Server"
+				user.update_attributes(
+					total_bet: 		profile_data["total_bet"],
+					coins_won: 		profile_data["coins_won"],
+					coins_lost: 	profile_data["coins_lost"],
+					total_spin: 	profile_data["total_spin"]
+				)
+			else
+				user.update_attributes(
+					stars: 					profile_data["stars"],
+					bet_index: 			profile_data["bet_index"],
+					total_coins: 		profile_data["total_coins"],
+					bet_per_line: 	profile_data["bet_per_line"],
+					current_level: 	profile_data["current_level"]
+				)
+			end
 		end
 	end
 
