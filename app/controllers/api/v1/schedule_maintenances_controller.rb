a8e898273902e9595b0db39703d931f747f2a34d
Amrendra Singh <amrendra.singh@w3villa.com>
2015-11-26 20:00:20 +0530

    SM-631 render added to fix missing template issue

--- app/controllers/api/v1/schedule_maintenances_controller.rb
+++ app/controllers/api/v1/schedule_maintenances_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::ScheduleMaintenancesController < Api::V1::ApplicationController
 	def sm
+		p "========== PUB-SUB FIRED ==========="
 		if params[:schedule_time].present?
 			REDIS_CLIENT.PUBLISH("server_maintenance", {publish_type: "server_maintenance", message: "Server is going on maintenance at "+params[:schedule_time]+" !"}.to_json)
 		else
 			REDIS_CLIENT.PUBLISH("kick_from_game", {publish_type: "kick_from_game", message: "Server is going on Maintenance!!"}.to_json)
 		end
+		render json: {
+			success: true
+		}
 	end
 end
