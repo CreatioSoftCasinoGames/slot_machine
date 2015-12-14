b12e1e4513bbfed10b69e8df4561e6f8343697d5
Amrendra Singh <amrendra.singh@w3villa.com>
2015-12-14 15:37:23 +0530

    Changed configuration for new sidekiq server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,15 @@
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://localhost:6379/' }
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'redis://localhost:6379/' }
+# end
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis.slits.online:6379/' }
+  config.redis = { url: 'redis://sidekiq.qlo4xg.0001.usw2.cache.amazonaws.com:6379/' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis.slits.online:6379/' }
+  config.redis = { url: 'redis://sidekiq.qlo4xg.0001.usw2.cache.amazonaws.com:6379/' }
 end
